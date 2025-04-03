e55edffb0adaa900aad604149c6b1714657dbbf6
root <sushi.honda@gmail.com>
2025-04-03 10:08:39 +0800

    ex00: 1st push std::transform
      Dont want to use loop(while, for...)
      New concept
      - container as new array
      - transform() as operate object

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,13 +6,26 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/31 13:39:48 by athonda           #+#    #+#             */
-/*   Updated: 2025/03/31 13:41:03 by athonda          ###   ########.fr       */
+/*   Updated: 2025/04/03 10:06:25 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cctype>
+
+std::string	my_toupper(std::string s)
+{
+	std::transform(s.begin(), s.end(), s.begin(), &::toupper);
+	return (s);
+}
 
 int	main(int ac, char **av)
 {
+	std::vector<std::string> args(av + 1, av + ac);
+	std::transform(args.begin(), args.end(), args.begin(), &my_toupper);
+	std::cout << args[0] << std::endl;
 	return (0);
 }
