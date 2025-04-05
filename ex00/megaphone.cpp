673634ea580634000191bb5856a7ba377ae712b7
root <sushi.honda@gmail.com>
2025-04-05 09:39:18 +0800

    add: my_join function
      looked for string join function or method but all of them join 2 elements,
      not possible to include delimita between them
    
      Point:
      1. accumulate() retuens std::string
      2. std::string can be joined by "+" wow!

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,15 +6,16 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/31 13:39:48 by athonda           #+#    #+#             */
-/*   Updated: 2025/04/03 14:22:40 by athonda          ###   ########.fr       */
+/*   Updated: 2025/04/05 09:36:50 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <cctype>
+#include <iostream>		//cout
+#include <vector>		//vector container
+#include <string>		//string
+#include <algorithm>	//stansform()
+#include <cctype>		//toupper
+#include <numeric>		//accumulate()
 
 std::string	my_toupper(std::string s)
 {
@@ -22,10 +23,21 @@ std::string	my_toupper(std::string s)
 	return (s);
 }
 
+std::string	my_join(std::string a, std::string b)
+{
+	std::string result;
+
+	result = a + ' ' + b;
+	return (result);
+}
+
 int	main(int ac, char **av)
 {
+	std::string	megaphone;
+
 	std::vector<std::string> args(av + 1, av + ac);
 	std::transform(args.begin(), args.end(), args.begin(), &my_toupper);
-	std::cout << args[1] << std::endl;
+	megaphone = std::accumulate(args.begin() + 1, args.end(), args[0], &my_join);
+	std::cout << megaphone << std::endl;
 	return (0);
 }
