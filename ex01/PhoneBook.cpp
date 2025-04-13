d884d7d40efec07c9dd310970494cd76607a8e59
root <sushi.honda@gmail.com>
2025-04-14 01:21:16 +0800

    add: display table form
      In SEARCH method in PhoneBook.cpp, call the other function
      because the function will be a little longer
      to set field size 10, std::setw(10) can be usefull

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -6,13 +6,15 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/13 13:22:37 by athonda           #+#    #+#             */
-/*   Updated: 2025/04/13 19:02:58 by athonda          ###   ########.fr       */
+/*   Updated: 2025/04/14 01:17:45 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
+#include <iomanip>
 #include "PhoneBook.hpp"
+#include "Contact.hpp"
 
 PhoneBook::PhoneBook()
 {
@@ -55,19 +57,55 @@ void PhoneBook::searchContact() const
 		std::cout << "Phonebook is empty." << std::endl;
 		return ;
 	}
-	contacts[0].displayDetails();
-	contacts[1].displayDetails();
-	contacts[2].displayDetails();
-	contacts[3].displayDetails();
-	contacts[4].displayDetails();
-	contacts[5].displayDetails();
-	contacts[6].displayDetails();
-	contacts[7].displayDetails();
+	displayContacts();
+
+	int	index;
+	std::cout << "Enter index to display detail [0-" << count - 1 << "]: ";
+	std::cin >> index;
+
+	contacts[index].displayDetails();
+//	contacts[0].displayDetails();
+//	contacts[1].displayDetails();
+//	contacts[2].displayDetails();
+//	contacts[3].displayDetails();
+//	contacts[4].displayDetails();
+//	contacts[5].displayDetails();
+//	contacts[6].displayDetails();
+//	contacts[7].displayDetails();
 }
 
-void displayContacts()
+void PhoneBook::displayContacts() const
 {
+	std::cout << std::setw(10) << "Index" << "|";
+	std::cout << std::setw(10) << "First Name" << "|";
+	std::cout << std::setw(10) << "Last Name" << "|";
+	std::cout << std::setw(10) << "Nickname" << std::endl;
 
+	std::cout << std::string(43, '-') << std::endl;
+
+	for (int i = 0; i < count; i++)
+	{
+		std::cout << std::setw(10) << i << "|";
+
+		std::string firstName = contacts[i].getFirstName();
+		if (firstName.length() > 10)
+		{
+			firstName = firstName.substr(0, 9) + ".";
+		}
+		std::cout << std::setw(10) << firstName << "|";
+		std::string lastName = contacts[i].getLastName();
+		if (lastName.length() > 10)
+		{
+			lastName = lastName.substr(0, 9) + ".";
+		}
+		std::cout << std::setw(10) << lastName << "|";
+		std::string nickName = contacts[i].getNickName();
+		if (nickName.length() > 10)
+		{
+			nickName = nickName.substr(0, 9) + ".";
+		}
+		std::cout << std::setw(10) << nickName << std::endl;
+	}
 }
 
 void displayContactDetails(int index)
