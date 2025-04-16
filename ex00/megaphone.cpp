a683f069795c377a0bf466dd1a0da3ef26693641
root <sushi.honda@gmail.com>
2025-04-16 18:17:48 +0800

    revise: adjust to subject simply
      deeeeply reading the subject and examples, no need to trim spaces in each argument, and no need to combine args with spaces. Just connect all inputs....
      1. delete trim
      2. delete cout << " "
    
      Then, no memory leak!!

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,33 +6,13 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/31 13:39:48 by athonda           #+#    #+#             */
-/*   Updated: 2025/04/10 14:55:54 by athonda          ###   ########.fr       */
+/*   Updated: 2025/04/16 18:16:17 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>		//cout
 #include <string>		//string, find_first_not_of(), find_last_not_of()
 #include <cctype>		//toupper()
-#include <numeric>		//accumulate()
-
-/**
- * @fn my_trim
- * @brief truncate string into substring
- * @param[in] string
- * @return string
- */
-
-std::string	my_trim(std::string s)
-{
-	std::string::size_type	first;
-	std::string::size_type	last;
-	std::string trimed;
-
-	first = s.find_first_not_of(" \t\n\r\f\v");
-	last = s.find_last_not_of(" \t\n\r\f\v");
-	trimed = s.substr(first, last - first + 1);
-	return (trimed);
-}
 
 int	main(int ac, char **av)
 {
@@ -47,18 +27,12 @@ int	main(int ac, char **av)
 	for (i = 1; i < ac; ++i)
 	{
 		std::string	s(av[i]);
-		s = my_trim(s);
 		for (j = 0; s[j] != '\0'; ++j)
 		{
 			s[j] = std::toupper(static_cast<unsigned char>(s[j]));
 		}
 		std::cout << s;
-		if (i < ac - 1)
-		{
-			std::cout << " ";
-		}
-		else
-			std::cout << std::endl;
 	}
+	std::cout << std::endl;
 	return (0);
 }
