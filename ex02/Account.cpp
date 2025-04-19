cb10a42cc28a6d2fe304daf317941d59ed48885d
root <sushi.honda@gmail.com>
2025-04-19 18:17:39 +0800

    add: timestamp and main.cpp for testing
      timestamp is made from time() and localtime()
      to confirm the time stamp data, in main() make small instance with initial deposit.
      displaytimestamp function is private method. so public method call the displaytimestamp

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -6,19 +6,20 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 04:23:30 by athonda           #+#    #+#             */
-/*   Updated: 2025/04/19 16:47:08 by athonda          ###   ########.fr       */
+/*   Updated: 2025/04/19 17:55:23 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>			// std::cout
 #include <ctime>			// std::time_t, std::localtime()
+#include <iomanip>			// std::setw, std::setfill()
 #include "Account.hpp"
 
 // intializing static variables
-static int	_nbAccounts = 0;
-static int	_totalAmount = 0;
-static int	_totalNbDeposits = 0;
-static int	_totalNbWithdrawals = 0;
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
 
 // Constructor
 Account::Account(int initial_deposit) :
@@ -37,48 +38,52 @@ Account::Account(int initial_deposit) :
 }
 
 // destructor
-~Account()
+Account::~Account(void)
+{}
 
 // private constructor (no need)
 Account::Account(void)
-{
-
-}
+{}
 
 // getter
-static int	Account::getNbAccounts(void)
+int	Account::getNbAccounts(void)
 {
 	return (_nbAccounts);
 }
 
-static int	Account::getTotalAmount(void)
+int	Account::getTotalAmount(void)
 {
 	return (_totalAmount);
 }
 
-static int	Account::getNbDeposits(void)
+int	Account::getNbDeposits(void)
 {
 	return (_totalNbDeposits);
 }
 
-static int	Account::getNbWithdrawals(void)
+int	Account::getNbWithdrawals(void)
 {
-	return (_totalnbWithdrawals);
+	return (_totalNbWithdrawals);
 }
 
-static void	Account::displayAccountsInfos(void)
+void	Account::displayAccountsInfos(void)
 {
-
+	_displayTimestamp();
+	std::cout << "accounts:" << _nbAccounts << ";";
+	std::cout << "total:" << _totalAmount << ";";
+	std::cout << "deposits:" << _totalNbDeposits << ";";
+	std::cout << "withdrawals:" << _totalNbWithdrawals;
+	std::cout << std::endl;
 }
 
 // time set function
-static void	Account::_displayTimestamp(void)
+void	Account::_displayTimestamp(void)
 {
 	std::time_t	now = std::time(NULL);
 	std::tm *tm = std::localtime(&now);
 
 	std::cout << "[";
-	std::cout << std::setfill('0') << std::setw(4) << tm->tm_year + 1900;
+	std::cout << std::setfill('0') << std::setw(6) << tm->tm_year + 1900;
 	std::cout << "]";
 
 }
---
+++ ex02/main.cpp
@@ -0,0 +1,9 @@
+#include "Account.hpp"
+
+int	main(void)
+{
+	Account acc(100);
+	Account::displayAccountsInfos();
+
+	return (0);
+}
